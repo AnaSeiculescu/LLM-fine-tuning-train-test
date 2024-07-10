4755988fc4c9f21a2a607f7a22f35029c4dd0216
Ana Seiculescu <anaseiculescu@gmail.com>
2024-07-10 12:02:36 +0300

    adding a System prompt message to the train examples, converting all 16 examples to jsonl. example 6 and 14 will help for testing

--- docx-to-jsonl/index.js
+++ docx-to-jsonl/index.js
@@ -11,8 +11,17 @@ async function docxToJsonl(docxFilePath, jsonlFilePath) {
         const trimmedContent = docxContent.trim();
 
         // String processing according to the Llama 2 model template
-        let processedContent = `<s>[INST]${trimmedContent}</s>`;
-        processedContent = processedContent.replace("Oferta pentru firma", "[/INST]Oferta pentru firma");
+        let processedContent = `<s>[INST]<<SYS>>Answer only in the Romanian language. Raspunde numai in limba romana.
+Raspunsul trebuie sa fie un plan de dezvoltare a unei aplicatii web sau mobile, in functie de necesitate.
+Raspunsul trebuie sa tina cont de cerintele clientului. Planul de development va fi aplicat de firma de IT. 
+Domeniul de activitate al firmei este Software Development.
+<</SYS>>Urmatorul este cerinta clientului:${trimmedContent}</s>`;
+
+        const processedContentReplacement = `Pe baza acestora formuleaza un plan de dezvoltare a unei aplicatii web sau 
+mobile, in functie de necesitate. Planul de dezvoltare are 4 sectiuni principale: "1.Scopul documentului", 
+"2.Structura proiectului", "3.Sugestii suplimentare" si "4.Pret si timp de implementare".[/INST]Oferta pentru firma`;
+
+        processedContent = processedContent.replace("Oferta pentru firma", processedContentReplacement);
 
         // Create a single JSON object
         const jsonObject = { text: processedContent };
@@ -36,7 +45,7 @@ async function docxToJsonl(docxFilePath, jsonlFilePath) {
 let docxFilePath;
 let jsonlFilePath;
 
-for (let i = 1; i < 15; i++) {
+for (let i = 1; i < 17; i++) {
     docxFilePath = path.join(__dirname, `OfertaTest${i}.docx`);
     jsonlFilePath = path.join(__dirname, `train${i}.jsonl`);
 
